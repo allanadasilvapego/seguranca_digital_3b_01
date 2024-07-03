63174af81cba889752a1ac62394d842d5571ece7
allanadasilvapego <127985302+allanadasilvapego@users.noreply.github.com>
2024-07-03 18:59:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.querySelector('.parametro-senha__botao');
 const numeroSenha = document.querySelector('.parametro-senha__texto'); 
-numeroSenha.textContent = " 5 ";
+numeroSenha.textContent =  5 ;
